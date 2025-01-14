f9b762ef5c5ea66d079a51e788df56ed0b8762a6
Pinkacai <eliel.j.levine@gmail.com>
2025-01-14 23:03:24 +0200

    2

--- Moduuli 1/Moduuli1.py
+++ Moduuli 1/Moduuli1.py
@@ -1,5 +1,18 @@
-User = input("What's your name?: ")
-print("Nice to meet you,  " + User + "!")
+#User = input("What's your name?: ")
+#print("Nice to meet you,  " + User + "!")
+import math
 
-#Number = input(" What's your favorite number?: ")
-#Print("Great! " + Number + " is a great choice")
+#Fahrenheit_str = input("Enter temperature in fahrenheit: ")
+#Fahrenheit = float(Fahrenheit_str)
+#Celcius = (Fahrenheit - 32)*5/9
+#print (f"The degrees in celcius are: {Celcius:.2f}")
+
+#<20s : a string printed into a field of 20 characters wide, justified to the left
+#8d : an integer into a field of 8 characters wide
+#WHAT???????^
+
+Radius_str = float(input("What is the radius of the circle?: "))
+R = float(Radius_str)
+Pi = float(math.pi)
+Area = float(Pi*(R**2))
+print ("The area is: " + str(Area))
