8a94eaa03eb07a7e886d4fd5a0e03b4a36d310f7
Pinkacai <eliel.j.levine@gmail.com>
2025-01-15 14:30:38 +0200

    2

---
+++ Moduuli 2/Tehtävä 3.py
@@ -0,0 +1,6 @@
+A = int(input("Input the length of the base: "))
+B = int(input("Input the length of the side: "))
+SidesA2 = (A)*2
+SidesB2 = (B)*2
+Circumferance = (SidesA2 + SidesB2)
+print("The circumferance is: " + str(Circumferance))
