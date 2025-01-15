71c77352006cfd36c8c158b0cc993696a009019b
Pinkacai <eliel.j.levine@gmail.com>
2025-01-15 12:48:33 +0200

    2

---
+++ EXTRAS/extra stuff.py
@@ -0,0 +1,16 @@
+#<20s : a string printed into a field of 20 characters wide, justified to the left
+#8d : an integer into a field of 8 characters wide
+#WHAT???????^
+
+
+
+#Area = int(input("Number?: "))
+#if Area != int(1):
+    #print("Cool " + str(Area))
+
+#else: print(" N000, not " + str(Area))
+
+#Fahrenheit_str = input("Enter temperature in fahrenheit: ")
+#Fahrenheit = float(Fahrenheit_str)
+#Celcius = (Fahrenheit - 32)*5/9
+#print (f"The degrees in celcius are: {Celcius:.2f}")
--- Moduuli 1/Moduuli1.py
+++ Moduuli 1/Moduuli1.py
@@ -2,17 +2,5 @@
 #print("Nice to meet you,  " + User + "!")
 import math
 
-#Fahrenheit_str = input("Enter temperature in fahrenheit: ")
-#Fahrenheit = float(Fahrenheit_str)
-#Celcius = (Fahrenheit - 32)*5/9
-#print (f"The degrees in celcius are: {Celcius:.2f}")
 
-#<20s : a string printed into a field of 20 characters wide, justified to the left
-#8d : an integer into a field of 8 characters wide
-#WHAT???????^
 
-Radius_str = float(input("What is the radius of the circle?: "))
-R = float(Radius_str)
-Pi = float(math.pi)
-Area = float(Pi*(R**2))
-print ("The area is: " + str(Area))
