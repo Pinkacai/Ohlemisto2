7ba95529b1998287d71611906bb10f1769a2005e
Pinkacai <eliel.j.levine@gmail.com>
2025-01-15 18:46:43 +0200

    2

--- Moduuli 2/Tehtävä 3.py
+++ Moduuli 2/Tehtävä 3.py
@@ -2,5 +2,6 @@ A = float(input("Input the length of the base: "))
 B = float(input("Input the length of the side: "))
 SidesA2 = (A)*2
 SidesB2 = (B)*2
-Circumferance = (SidesA2 + SidesB2)
-print("The circumferance is: " + str(Circumferance))
+Perimeter = (SidesA2 + SidesB2)
+Area = (A)*(B)
+print("The perimeter is: " + str(Perimeter) + ". And the area is: " + str(Area))
