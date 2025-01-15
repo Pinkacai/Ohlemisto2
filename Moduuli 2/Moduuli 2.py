35faa15a18c67c96f9dd30dbd5355ec62421cbe0
Pinkacai <eliel.j.levine@gmail.com>
2025-01-15 12:45:05 +0200

    2

--- Moduuli 2/Moduuli 2.py
+++ Moduuli 2/Moduuli 2.py
@@ -1,1 +1,5 @@
-print("Hello")
+Radius_str = float(input("What is the radius of the circle?: "))
+R = float(Radius_str)
+Pi = float(math.pi)
+Area = float(Pi*(R**2))
+print ("The area is: " + str(Area))
