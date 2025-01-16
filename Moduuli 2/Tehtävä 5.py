70ffb15d7d83a2b5ab94a9295095051b251fc73b
Pinkacai <eliel.j.levine@gmail.com>
2025-01-16 13:53:38 +0200

    2

--- Moduuli 2/Tehtävä 5.py
+++ Moduuli 2/Tehtävä 5.py
@@ -1,23 +1,25 @@
 print(" ")
 
 gram = 1
-bul = (gram)*13.3
-nail = (bul)*32
-lei = (nail)*20
+bul = gram*13.3
+nail = bul*32
+lei = nail*20
 
 L = float(input("How many leiviskät?: "))
 N = float(input("How many nails?: "))
 B = float(input("How many bullets?: "))
 
-L2 = (L)*(lei)
-N2 = (N)*(nail)
-B2 = (B)*(bul)
-weight =(L2)+(N2)+(B2)
+L2 = L*lei
+N2 = N*nail
+B2 = B*bul
+weight =L2+N2+B2
 
-kg = int((weight)/1000)
-g = int((weight)%1000)
+kg = int(weight/1000)
+g = int(weight%1000)
 
 print(" ")
-print(f"The equivalent weight in kilograms and grams is: {kg:.0f}" "kg and " + (f"{g:0.1f}" "g."))
+print(f"The equivalent weight in kilograms and grams is: {kg:.0f}" "kg and " + f"{g:0.1f}" "g.")
+
+
 
 
