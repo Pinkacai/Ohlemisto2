e45cfb97472138c735b4bce3d3391845a054c4ed
Pinkacai <eliel.j.levine@gmail.com>
2025-01-16 11:53:31 +0200

    2

---
+++ Moduuli 2/Tehtävä 6.py
@@ -0,0 +1,14 @@
+import random
+
+R= random.randint(0,9)
+R2= random.randint(0,9)
+R3= random.randint(0,9)
+
+R4=random.randint(1,6)
+R5=random.randint(1,6)
+R6=random.randint(1,6)
+R7=random.randint(1,6)
+
+print("  ")
+print("Your three digit code is ", R, R2, R3 , ". ")
+print("Your four digit code is " , R4, R5, R6, R7 , ". ")
