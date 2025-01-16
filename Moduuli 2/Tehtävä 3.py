f749ce604013c7fcb36cd8234dc3ed7d2abff7bf
Pinkacai <eliel.j.levine@gmail.com>
2025-01-16 14:15:46 +0200

    2

--- Moduuli 2/Tehtävä 3.py
+++ Moduuli 2/Tehtävä 3.py
@@ -1,7 +1,7 @@
 A = float(input("Input the length of the base: "))
 B = float(input("Input the length of the side: "))
-SidesA2 = (A)*2
-SidesB2 = (B)*2
+SidesA2 = A*2
+SidesB2 = B*2
 Perimeter = (SidesA2 + SidesB2)
-Area = (A)*(B)
+Area = A*B
 print("The perimeter is: " + str(Perimeter) + ". And the area is: " + str(Area))
--- Moduuli 2/Tehtävä 4.py
+++ Moduuli 2/Tehtävä 4.py
@@ -6,11 +6,11 @@ num2 = float(input("Please input the second number: "))
 num3 = float(input("Please input the third and final number: "))
 print(" ")
 
-sum = (num1) + (num2) + (num3)
-mult = (num1)*(num2)*(num3)
-avrg = (sum)/3
+Sum = num1 + num2 + num3
+mult = num1*num2*num3
+avrg = Sum/3
 
-print(f"The sum of your numbers is: {sum:2.0f}")
+print(f"The sum of your numbers is: {Sum:2.0f}")
 print(f"The multiplication of your numbers is: {mult:2.0f}")
 print(f"The average of your numbers is: {avrg:2.0f}")
 
