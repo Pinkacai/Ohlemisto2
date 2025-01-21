3d459c734862864efd3a0de19b73b4fd3ba1c8af
Pinkacai <eliel.j.levine@gmail.com>
2025-01-21 14:29:15 +0200

    2

--- Moduuli 3/Tehtävä 1.py
+++ Moduuli 3/Tehtävä 1.py
@@ -6,6 +6,6 @@ size_dif = 42-fish_size
 print(" ")
 
 if fish_size <= 42:
-    print("You should release the fish back to the lake, it is " + str(size_dif) + "cm too small.")
+    print("You should release the fish back to the lake, it is "  + str(size_dif) + "cm too small.")
 else:
     print("That's a great catch!")
