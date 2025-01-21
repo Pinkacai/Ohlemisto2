98275ccf01aefb09ed3c54c7d91265960fa28569
Pinkacai <eliel.j.levine@gmail.com>
2025-01-21 15:14:32 +0200

    2

--- Moduuli 3/Tehtävä 4.py
+++ Moduuli 3/Tehtävä 4.py
@@ -1,11 +1,11 @@
 print(" ")
-print("This program will tell you if the year you input is a gap year")
-print(" ")
+print("This program will tell you if the year you input is a leap year")
 year = int(input("Please enter a year: "))
+print(" ")
 d_4 = year%4
 d_100 = year%100 == 0
 d_400 = year%400 == 0
 if d_4 == 0 and d_100 == d_400 :
-    print(str(year) + " is a gap year! ")
+    print(str(year) + " is a leap year! ")
 else:
-    print(str(year) + " is not a gap year")
+    print(str(year) + " is not a leap year")
