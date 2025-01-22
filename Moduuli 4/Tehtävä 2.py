5a9a618b6d636dd5c7da8cf78bc287171be2d0c1
Pinkacai <eliel.j.levine@gmail.com>
2025-01-22 14:59:53 +0200

    2

---
+++ Moduuli 4/Tehtävä 2.py
@@ -0,0 +1,10 @@
+print(" ")
+
+inches0= float(input("How many inches would you like to see converted into cm?: "))
+#inches2cm = inches0 * 2.54
+while inches0 >= 0:
+    print(str(inches0) + " inches is "  + str(inches0 * 2.54) + "cm")
+    inches0 = float(input("How many inches would you like to see converted into cm?: "))
+print(" ")
+print("That won't work here.")
+
