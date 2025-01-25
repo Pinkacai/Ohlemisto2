a0f91181f9367eb360d9ea7f07d2bca70fb3582a
Pinkacai <eliel.j.levine@gmail.com>
2025-01-25 14:00:01 +0200

    2

--- Moduuli 4/Tehtävä 3.py
+++ Moduuli 4/Tehtävä 3.py
@@ -1,29 +1,21 @@
-#import math
-
-#min()
-#max()
 print(" ")
 print("Please enter numbers as you please. To quit, enter an empty string. ")
 print(" ")
 
+big = None
+small = None
 
-
-
-big = small = 0
 while True:
     nums = input("Enter a number: ")
     if nums == "":
         print(f"The biggest number you entered was {big} and the smallest was {small}.")
         break
     else:
-        if big == 0:
-            big = nums
-        if small == 0:
-            small = nums
-        if nums < small:
-            small = nums
-        if nums > big:
-            big = nums
+        num = float(nums)
+        if big is None or num > big:
+            big = num
+        if small is None or num < small:
+            small = num
 
 
 
