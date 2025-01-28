49cfd6e07353ead5e41880a5569b8750d0f70aca
Pinkacai <eliel.j.levine@gmail.com>
2025-01-28 14:32:33 +0200

    2

---
+++ Moduuli 5/Tehtävä 1.py
@@ -0,0 +1,34 @@
+import random
+
+rolls = []
+roll = int(input("How many dice would you like to throw?: " ))
+outcome = 0
+for n in range(0, roll):
+    dice = random.randint(1,6)
+    rolls.append(dice)
+
+for n in rolls:
+    outcome = outcome + n
+
+print(outcome)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
