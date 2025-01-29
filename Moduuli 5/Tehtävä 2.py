8a153b650d6163b852666084f1a176f900676dcd
Pinkacai <eliel.j.levine@gmail.com>
2025-01-29 13:53:11 +0200

    2

--- Moduuli 5/Tehtävä 2.py
+++ Moduuli 5/Tehtävä 2.py
@@ -5,7 +5,8 @@ while numbers != "":
     num.append(numbers2)
     numbers = input("Please enter a number or enter an empty string to quit: ")
 
-
+print(" ")
+print("The five largest digits you entered were:")
 num.sort(reverse=True)
 for index in range(0,5):
     print(num[index])
