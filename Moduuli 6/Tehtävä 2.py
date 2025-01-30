d2f5e3c3e8cf5db1edd6d79541a94757d9dcd53b
Pinkacai <eliel.j.levine@gmail.com>
2025-01-30 10:16:54 +0200

    2

--- Moduuli 6/Tehtävä 2.py
+++ Moduuli 6/Tehtävä 2.py
@@ -4,9 +4,9 @@ main_num = 0
 def rolls(sides):
     ran = random.randint(1, sides)
     return ran
-sides = int(input("How many sides would you like your die to have?: "))
+sides = int(input("How many sides would you like your dice to have?: "))
 while main_num != int(sides):
     main_num = rolls(sides)
     print(f"You threw a {main_num}!")
 else:
-    print(f"You did it! You threw the biggest number in your {sides}!")
+    print(f"You did it! You threw the biggest number in your dice with {sides} sides!")
