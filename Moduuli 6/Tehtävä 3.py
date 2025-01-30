2461227784747af53f62ad327bfca81adc88e9a5
Pinkacai <eliel.j.levine@gmail.com>
2025-01-30 10:47:38 +0200

    2

--- Moduuli 6/Tehtävä 3.py
+++ Moduuli 6/Tehtävä 3.py
@@ -1,16 +1,20 @@
 print(" ")
 def gallons(amt):
     litres = amt * 4.54609
-    print(f"{amt:.2f} gallons is:{litres: .2f} litres.")
-    print(" ")
+    if amt < 0:
+        print("The amount you entered was negative. Goodbye")
+        quit()
+    else:
+        print(f"{amt:.2f} gallons is:{litres: .2f} litres.")
+        print(" ")
     return litres
 amt = float(input("How many gallons would you like to convert to litres?: "))
-print(" ")
 gallons(amt)
+print(" ")
 while amt > 0:
     amt = float(input("How many gallons would you like to convert to litres?: "))
     gallons(amt)
+    print(" ")
 else:
-    print("The amount you entered was negative. Goodbye")
     quit()
 
