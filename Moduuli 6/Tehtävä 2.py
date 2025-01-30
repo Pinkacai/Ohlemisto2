8a0e84b450ea1438e3bf5cb1f3348b609bd8a22b
Pinkacai <eliel.j.levine@gmail.com>
2025-01-30 10:15:22 +0200

    2

---
+++ Moduuli 6/Tehtävä 2.py
@@ -0,0 +1,12 @@
+import random
+main_num = 0
+
+def rolls(sides):
+    ran = random.randint(1, sides)
+    return ran
+sides = int(input("How many sides would you like your die to have?: "))
+while main_num != int(sides):
+    main_num = rolls(sides)
+    print(f"You threw a {main_num}!")
+else:
+    print(f"You did it! You threw the biggest number in your {sides}!")
