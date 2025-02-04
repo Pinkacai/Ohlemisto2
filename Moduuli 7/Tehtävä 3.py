22227c22a0efe982264f6a05f913a13a07ab34ca
Pinkacai <eliel.j.levine@gmail.com>
2025-02-04 18:22:43 +0200

    2

--- Moduuli 7/Tehtävä 3.py
+++ Moduuli 7/Tehtävä 3.py
@@ -5,33 +5,37 @@ command = int(input("Please choose one."
                 "\n9. quit."
                 "\nEnter a command: "))
 print("--------------------------------------------------")
-
-while command == 1:
-    icao = input("Please enter the ICAO code of the airport you wish to enter: ")
-    airport = input("Please enter the name of the airport you wish to enter: ")
-    airports[icao] = airport
-    command = int(input("Please choose one."
+while int(command in range(1,9)):
+    if command == 1:
+        icao = input("Please enter the ICAO code of the airport you wish to enter: ")
+        airport = input("Please enter the name of the airport you wish to enter: ")
+        airports[icao] = airport
+        print("--------------------------------------------------")
+        command = int(input("Please choose one."
                 "\n1. Enter a new airport."
                 "\n5. Fetch an existing airport name."
                 "\n9. quit."
                 "\nEnter a command: "))
-    print("--------------------------------------------------")
-while command == 5:
-    icao = input("Please enter the ICAO code of the airport you wish to see: ")
-    if icao in airports:
-        print(f"{icao} is the airport code for {airport[icao]}")
-    command = int(input("Please choose one."
+    if command == 5 :
+        icao = input("Please enter the ICAO code of the airport you wish to see: ")
+        if icao in airports:
+            print("")
+            print(f"{icao} is the airport code for {airports[icao]}")
+            print("--------------------------------------------------")
+            command = int(input("Please choose one."
                         "\n1. Enter a new airport."
                         "\n5. Fetch an existing airport name."
                         "\n9. quit."
                         "\nEnter a command: "))
-    print("--------------------------------------------------")
-while command == 9:
-    print(airports)
-    quit()
+        if icao not in airports:
+            print(" ")
+            print("ICAO code not found. please try again.")
+            print("--------------------------------------------------")
+            command = int(input("Please choose one."
+                        "\n1. Enter a new airport."
+                        "\n5. Fetch an existing airport name."
+                        "\n9. quit."
+                        "\nEnter a command: "))
+        if command == 9:
+            quit()
 
-
-
-
-
-print(airports)
