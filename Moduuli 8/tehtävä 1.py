25cb3f0e19fef4f23a6f87899d193b35fcc63a83
Pinkacai <eliel.j.levine@gmail.com>
2025-02-06 12:49:19 +0200

    2

---
+++ Moduuli 8/tehtävä 1.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='eliell2',
+         password='gr0ups',
+         autocommit=True
+         )
+
+def airport_info(icao):
+    sql = f"SELECT name FROM airport WHERE ident='{icao}'"
+    #print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount >0 :
+        for row in result:
+            print(f"The name of the airport is: {row[0]} ")
+    else:
+        print("The airport code you entered could not be found.")
+    return
+
+airport_code = input("Please enter the ICAO code of the airport you wish to see: ")
+airport_info(airport_code)
+
+
+
